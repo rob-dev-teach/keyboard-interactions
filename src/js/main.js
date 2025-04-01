e4a9a1474ef0363a65001048a99416b05b64f61e
Robin <robin.devouge@gmail.com>
2025-04-01 12:43:23 +0200

    Add movement on key press

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
-console.log('You got shreked!');
+import { gsap } from 'gsap';
+
+const character = {
+	element: document.querySelector('.character'),
+	position: {
+		x: 0,
+		y: 0,
+	},
+	speed: 3,
+	velocity: 0,
+}
+
+const animate = () => {
+	character.position.x += character.speed * character.velocity * gsap.ticker.deltaRatio();
+	character.element.style.left = `${character.position.x}px`;
+}
+
+gsap.ticker.add(animate);
+
+document.addEventListener('keydown', (event) => {
+	if (event.key === 'ArrowRight') {
+		character.velocity = 1;
+	}
+	if (event.key === 'ArrowLeft') {
+		character.velocity = -1;
+	}
+});
+document.addEventListener('keyup', (event) => {
+	if (event.key === 'ArrowRight') {
+		character.velocity = 0;
+	}
+	if (event.key === 'ArrowLeft') {
+		character.velocity = 0;
+	}
+});
