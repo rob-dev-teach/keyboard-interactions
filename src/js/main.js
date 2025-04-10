9f99aaf4e173513d468aebe32d45c34714a22aa0
Robin <robin.devouge@gmail.com>
2025-04-10 10:21:03 +0200

    Add world border collisions

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import { gsap } from 'gsap';
 
 const character = {
 	element: document.querySelector('.character'),
+	hitbox: document.querySelector('.character__hitbox'),
 	position: {
 		x: 0,
 		y: 0,
@@ -10,8 +11,26 @@ const character = {
 	velocity: 0,
 };
 
+const world = {
+	limitLeft: 0,
+	limitRight: window.innerWidth,
+}
+
+const checkCollisions = () => {
+	const hitboxRect = character.hitbox.getBoundingClientRect();
+
+	if (hitboxRect.left < world.limitLeft) {
+		character.position.x = world.limitLeft;
+	} else if (hitboxRect.right > world.limitRight) {
+		character.position.x = world.limitRight - hitboxRect.width;
+	}
+};
+
 const animate = () => {
 	character.position.x += character.speed * character.velocity * gsap.ticker.deltaRatio();
+
+	checkCollisions();
+
 	character.element.style.left = `${character.position.x}px`;
 };
 
